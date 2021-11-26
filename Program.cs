3528cd210b15209bf293445c428df8b3d9f59d2b
Harut Safaryan <safaryanharut@gmail.com>
2021-11-26 15:40:07 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _Dictionary<string, int> dictionary = new _Dictionary<string, int>();
+            Dictionary<string, int> dictionary1 = new Dictionary<string, int>();
+            dictionary.Add("Hello", 10);
+            dictionary.Add("happy", 12);
+            dictionary.Add("year", 1);
+
+            int s = dictionary["happy"];
+
+
+            //foreach (var item in dictionary)
+            //{
+
+            //}
+            Console.WriteLine();
+        }
+    }
+}
